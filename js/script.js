427a7e078ed2c62b592217b5a3c35ab39fc5ffad
Ivan Nefagin <dfyz0r@list.ru>
2020-08-07 13:13:26 +0300

    вносит правки усложненного задания

--- js/script.js
+++ js/script.js
@@ -1,13 +1,8 @@
-let num = 266219,
-    multiply = 1;
+const num = 266219,
+    numDigits = String(num).split('').map(item => Number(item)),
+    product = numDigits.reduce((total, digit) => total * digit),
+    result = String(product ** 3).slice(0, 2);
 
-while (num > 0) {
-    multiply *= num % 10;
-    num = Math.trunc(num / 10);
-}
+console.log(product);
 
-console.log(multiply);
-
-let newNum = multiply ** 3;
-
-console.log(String(newNum).slice(0, 2));
+console.log(result);
