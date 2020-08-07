9807454966bc56a7cf867ed446b0fc4046cdd6dd
Ivan Nefagin <dfyz0r@list.ru>
2020-08-07 10:54:22 +0300

    завершает обязательное задание

--- js/script.js
+++ js/script.js
@@ -4,3 +4,18 @@ let money = 60000,
     deposit = true,
     mission = 40000,
     period = 4;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log('Период равен ' + period + ' месяца');
+console.log('Цель - заработать ' + mission + ' рублей');
+
+addExpenses.toLowerCase();
+console.log(addExpenses.split(', '));
+
+let budgetDay = money / 30;
+console.log(budgetDay);
