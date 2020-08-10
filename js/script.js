d28714e0b9ea7ebef421607d0d038c0db9f1ea89
Ivan Nefagin <dfyz0r@list.ru>
2020-08-10 12:02:34 +0300

    feat(script-lesson03-c): add task decision via if-else

--- js/script.js
+++ js/script.js
@@ -1,38 +1,29 @@
-const money = prompt('Ваш месячный доход?', '50000'),
-    income = 'инвестиции',
-    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую:', 'Квартира, Еда'),
-    expense1 = prompt('Введите обязательную статью расходов:', 'Квартира').toLowerCase(),
-    amount1 = prompt('Во сколько это обойдется?', '20000'),
-    expense2 = prompt('Введите обязательную статью расходов:', 'Еда').toLowerCase(),
-    amount2 = prompt('Во сколько это обойдется?', '10000'),
-    deposit = confirm('Нажмите "OK", если у Вас есть депозит в банке'),
-    budgetMonth = money - amount1 - amount2,
-    mission = 40000,
-    period = 4,
-    missionReach = Math.ceil(mission / budgetMonth),
-    budgetDay = Math.floor(budgetMonth / 30);
+const lang = prompt('Введите язык, на котором нужно вывести дни недели (ru/en):', 'ru'),
+    daysOfTheWeekRu = [
+        'Понедельник',
+        'Вторник',
+        'Среда',
+        'Четверг',
+        'Пятница',
+        'Суббота',
+        'Воскресенье'
+    ],
+    daysOfTheWeekEn = [
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+        'Sunday'
+    ];
 
-console.log(typeof money, typeof income, typeof deposit);
-
-console.log(addExpenses.length);
-
-console.log('Период равен ' + period + ' мес.');
-console.log('Цель - заработать ' + mission + ' рублей');
-
-console.log(addExpenses.toLowerCase().split(', '));
-
-console.log('Бюджет на месяц: ' + budgetMonth);
-
-console.log('Цель будет достигнута за: ' + missionReach + ' мес.');
-
-console.log('Бюджет на день: ' + budgetDay);
-
-if (budgetDay > 1200) {
-    console.log('У Вас высокий уровень дохода')
-} else if (budgetDay > 600) {
-    console.log('У Вас средний уровень дохода')
-} else if (budgetDay >= 0) {
-    console.log('К сожалению, у Вас уровень дохода ниже среднего')
+if (lang === 'ru') {
+    console.log('Дни недели на русском языке:');
+    daysOfTheWeekRu.forEach(item => console.log('\t' + item));
+} else if (lang === 'en') {
+    console.log('Дни недели на английском языке:');
+    daysOfTheWeekEn.forEach(item => console.log('\t' + item));
 } else {
-    console.log('Что-то пошло не так')
+    console.log('Ошибка при вводе');
 }
