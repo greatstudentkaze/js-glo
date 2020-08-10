aa973b1a905ddf014c99ece398887f19fcbddfa7
Ivan Nefagin <dfyz0r@list.ru>
2020-08-10 11:12:07 +0300

    feat(script): add mission achievement evaluation

--- js/script.js
+++ js/script.js
@@ -6,10 +6,11 @@ const money = prompt('Ваш месячный доход?'),
     expense2 = prompt('Введите обязательную статью расходов:'),
     amount2 = prompt('Во сколько это обойдется?'),
     deposit = confirm('Есть ли у Вас депозит в банке?'),
+    budgetMonth = money - amount1 - amount2,
     mission = 40000,
     period = 4,
-    budgetDay = money / 30,
-    budgetMonth = money - (amount1 + amount2);
+    missionReach = Math.ceil(mission / budgetMonth),
+    budgetDay = money / 30;
 
 console.log(typeof money, typeof income, typeof deposit);
 
@@ -17,6 +18,7 @@ console.log(addExpenses.length);
 
 console.log('Период равен ' + period + ' месяца');
 console.log('Цель - заработать ' + mission + ' рублей');
+console.log('Цель будет достигнута за: ' + missionReach + ' мес.');
 
 console.log(addExpenses.toLowerCase().split(', '));
 
