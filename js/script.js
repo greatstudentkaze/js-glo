1470948c3fa580c9bf0a452a5473f355e23ba59e
gsk <dfyz0r@list.ru>
2020-08-12 19:44:30 +0300

    feat(script-lesson05-c): add a solution to the second task

--- js/script.js
+++ js/script.js
@@ -6,3 +6,20 @@ arr.forEach(item => {
 });
 
 console.log('\n');
+
+// 2
+console.log('Все простые числа от 1 до 100:');
+for (let i = 1; i < 101; i++) {
+    if (isPrimeNumber(i)) console.log(`${i} (делители: ${1} и ${i})`)
+}
+
+function isPrimeNumber(n) {
+    if (n === 1) return false;
+    else if (n === 2) return true;
+
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) return false;
+    }
+
+    return true;
+}
