6afdfae2124c7c1ae30d7913893389062032b872
Ivan Nefagin <dfyz0r@list.ru>
2020-08-18 13:49:42 +0300

    chore(script-lesson09): add new project files

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const num = 266219,
-    numDigits = String(num).split('').map(item => Number(item)),
-    product = numDigits.reduce((total, digit) => total * digit),
-    result = String(product ** 3).slice(0, 2);
-
-console.log(product);
-
-console.log(result);
