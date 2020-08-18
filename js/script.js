eefc995a892bba9757d88dcd0a0a827ccabc7ea5
Ivan Nefagin <dfyz0r@list.ru>
2020-08-18 15:58:07 +0300

    feat(script-lesson09-c): add a solution to the first and second task

--- js/script.js
+++ js/script.js
@@ -1,8 +1,63 @@
-const num = 266219,
-    numDigits = String(num).split('').map(item => Number(item)),
-    product = numDigits.reduce((total, digit) => total * digit),
-    result = String(product ** 3).slice(0, 2);
+'use strict';
 
-console.log(product);
+const date = new Date();
 
-console.log(result);
+const declensionTime = (hour, min, sec) => {
+  if (hour % 10 === 1 && Math.floor(hour / 10) !== 1) {
+    hour += ' час';
+  } else if (hour % 10 >= 2 && hour % 10 <= 4 && Math.floor(hour / 10) !== 1) {
+    hour += ' часа';
+  } else {
+    hour += ' часов';
+  }
+
+  if (min % 10 === 1 && Math.floor(min / 10) !== 1) {
+    min += ' минута';
+  } else if (min % 10 >= 2 && min % 10 <= 4 && Math.floor(min / 10) !== 1) {
+    min += ' минуты';
+  } else {
+    min += ' минут';
+  }
+
+  if (sec % 10 === 1 && Math.floor(sec / 10) !== 1) {
+    sec += ' секунда';
+  } else if (sec % 10 >= 2 && sec % 10 <= 4 && Math.floor(sec / 10) !== 1) {
+    sec += ' секунды';
+  } else {
+    sec += ' секунд';
+  }
+
+  return hour + ' ' + min + ' ' + sec;
+};
+
+const addZero = (number) => {
+  if (Math.floor(number / 10) === 0) return Number('0' + number);
+  else return number;
+};
+
+let dateA = date.toLocaleString('ru', {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+});
+dateA = 'Сегодня ' + dateA[0].toUpperCase() + dateA.slice(1);
+dateA = dateA.replace('г.', 'года');
+
+const timeA = declensionTime(date.getHours(), date.getMinutes(), date.getSeconds()),
+  datetimeA = dateA + ', ' + timeA;
+
+const dateB = date.toLocaleString('ru', {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric'
+  }),
+  timeB = new Date().toLocaleString('ru', {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+  }),
+  datetimeB = dateB + ' - ' + timeB;
+
+document.body.insertAdjacentHTML('afterbegin', '<p>б) ' + datetimeB + '</p>');
+document.body.insertAdjacentHTML('afterbegin', '<p>а) ' + datetimeA + '</p>');
