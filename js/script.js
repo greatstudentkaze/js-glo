6da10bb873d89bce0bfcb84c102e0f1d2def8e7a
Ivan Nefagin <dfyz0r@list.ru>
2020-08-19 10:18:28 +0300

    feat(script-lesson10): arrange books

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const booksWrapper = document.querySelector('.books'),
+  books = booksWrapper.querySelectorAll('.book');
+
+booksWrapper.prepend(books[1]);
+booksWrapper.append(books[2]);
+books[3].before(books[4]);
