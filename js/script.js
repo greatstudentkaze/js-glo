5e9baaf2ebf44f6b59c4aa2440dbca25b9901f4f
Ivan Nefagin <dfyz0r@list.ru>
2020-08-19 10:47:48 +0300

    feat(script-lesson10): remove ad

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const booksWrapper = document.querySelector('.books'),
-  books = booksWrapper.querySelectorAll('.book');
+  books = booksWrapper.querySelectorAll('.book'),
+  adWrapper = document.querySelector('.adv');
 
 booksWrapper.prepend(books[1]);
 booksWrapper.append(books[2]);
@@ -10,3 +11,5 @@ books[3].before(books[4]);
 document.body.classList.add('background-image');
 
 books[4].getElementsByTagName('a')[0].textContent = 'Книга 3. this и Прототипы Объектов';
+
+adWrapper.remove();
