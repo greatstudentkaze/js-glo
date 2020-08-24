73c0617273dcba3d0fadb8a733b4b5ae081446da
Ivan Nefagin <dfyz0r@list.ru>
2020-08-24 15:12:01 +0300

    feat(script-lesson12-c): add task solution

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
-const num = 266219,
-    numDigits = String(num).split('').map(item => Number(item)),
-    product = numDigits.reduce((total, digit) => total * digit),
-    result = String(product ** 3).slice(0, 2);
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-console.log(product);
+function getRandomColor() {
+  let color = getRandomIntInclusive(0x000000, 0xffffff).toString(16);
 
-console.log(result);
+  if (color.length < 6) {
+    for (let i = 0; i < 6 - color.length; i++) {
+      color = '0' + color;
+    }
+  }
+
+  return color;
+}
+
+const color = document.querySelector('.color'),
+  changeColorBtn = document.querySelector('.color-change');
+
+changeColorBtn.addEventListener('click', () => {
+  const newColor = getRandomColor();
+
+  document.body.style.backgroundColor = `#${newColor}`;
+  color.textContent = `#${newColor}`;
+  color.style.color = `#${newColor}`;
+  changeColorBtn.style.color = `#${newColor}`;
+});
